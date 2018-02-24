20ad67320bd15d019e31a2ab44ba9303cec9b2e7
Rahel Lüthy <rahel.luethy@gmail.com>
2018-02-24 11:41:57 +0100

    Add CORS headers

--- server/src/main/scala/ch/netzwerg/demo/server/ServerApp.scala
+++ server/src/main/scala/ch/netzwerg/demo/server/ServerApp.scala
@@ -2,13 +2,14 @@ package ch.netzwerg.demo.server
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.stream.ActorMaterializer
-import akka.http.scaladsl.server._
-import akka.http.scaladsl.server.Directives._
-import spray.json._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server._
+import akka.stream.ActorMaterializer
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
+import spray.json._
 
-import Console._
+import scala.Console._
 import scala.concurrent.Await
 import scala.language.postfixOps
 
@@ -20,6 +21,7 @@ object ServerApp extends App {
   implicit val materializer = ActorMaterializer()
 
   import actorSystem.dispatcher
+
   import scala.concurrent.duration._
 
   logger("Starting GRAPHQL server...")
@@ -27,14 +29,15 @@ object ServerApp extends App {
   //shutdown Hook
   scala.sys.addShutdownHook(() -> shutdown())
 
-  val route: Route =
-    (post & path("graphql")){
-      entity(as[JsValue]){ requestJson =>
+  val route: Route = cors() {
+    (post & path("graphql")) {
+      entity(as[JsValue]) { requestJson =>
         GraphQLServer.endpoint(requestJson)
       }
     } ~ {
       getFromResource("graphiql.html")
     }
+  }
 
   Http().bindAndHandle(route, "0.0.0.0", PORT)
 
